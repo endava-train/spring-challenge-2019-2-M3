5aa0f07f8afa6bf9d18beeda9dbdbd92218a0bae
jhont285 <jhont285@gmail.com>
2019-08-28 11:26:19 -0500

    initial commit

---
+++ src/main/java/com/endava/third/ThirdMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.endava.third;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdMicroserviceApplication.class, args);
+	}
+
+}
